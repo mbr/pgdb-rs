60cb3e1d7e7c4d9e61176cba598189a2309214b5
Marc Brinkmann <git@marcbrinkmann.de>
2021-06-12 13:52:06 +0200

    Add support for `pgdb_cli` command line app

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # CHANGELOG
 
+## [Unreleased]
+
+### Added
+
+- CLI tool `pgdb` that allows running Postgres instances from the command line.
+
+### Changed
+
+- Repository is now multi-crate.
+
 ## [0.1.0] - 2021-06-12
 
 ### Added
--- pgdb_cli/src/main.rs
+++ pgdb_cli/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread, time::Duration};
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opts {}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::from_args();
+
+    let _pg = pgdb::Postgres::build().start()?;
+
+    thread::sleep(Duration::from_millis(500));
+    println!();
+    println!("Postgres is now running and ready to accept connections");
+    loop {
+        thread::sleep(Duration::from_secs(60));
+    }
 }
