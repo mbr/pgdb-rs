c7bf09c183d8004d5923b81a52d0b9176a83a6b7
Marc Brinkmann <git@marcbrinkmann.de>
2025-08-02 11:40:34 +0200

    Pin nixos-25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "pgdb - A Rust project with PostgreSQL";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
