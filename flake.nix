a8039997d601450af257ee2219a24dcdcf309660
Marc Brinkmann <git@marcbrinkmann.de>
2025-08-02 11:32:00 +0200

    Draft first `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "pgdb - A Rust project with PostgreSQL";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        
+        toolchain = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ];
+        
+        platform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+        
+        cargoToml = pkgs.lib.importTOML ./Cargo.toml;
+      in
+      {
+        packages.default = platform.buildRustPackage rec {
+          pname = "pgdb";
+          version = cargoToml.workspace.package.version;
+          nativeBuildInputs = with pkgs; [ postgresql ];
+          
+          src = pkgs.lib.cleanSource ./.;
+          
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            toolchain
+            postgresql
+            # Add any other development tools you need
+            cargo-watch
+            rust-analyzer
+          ];
+        };
+      });
+}
